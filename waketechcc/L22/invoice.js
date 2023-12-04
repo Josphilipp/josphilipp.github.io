148fdb0a870b1ee78889956228b833fca06a693e
Josphilipp <jphilippe@my.waketech.edu>
2023-12-04 17:32:55 -0500

    push

--- waketechcc/L22/invoice.js
+++ waketechcc/L22/invoice.js
@@ -26,48 +26,55 @@ function createInvoice() {
     // Generate invoice content using template literals
     const invoiceContent = `
       <h1>Customer Invoice</h1>
-  
-      <p>
-        Customer Name: ${name}<br>
-        Street: ${street}<br>
-        City: ${city}, ${state} ${zip}<br>
-        Phone: ${phone}
-      </p>
-  
       <table>
-        <tr>
-          <th>Item</th>
-          <th>Product</th>
-          <th>Price</th>
-        </tr>
-        <tr>
-          <td>1</td>
-          <td>${item1}</td>
-          <td>$${price1}</td>
-        </tr>
-        <tr>
-          <td>2</td>
-          <td>${item2}</td>
-          <td>$${price2}</td>
-        </tr>
-        <tr>
-          <td>3</td>
-          <td>${item3}</td>
-          <td>$${price3}</td>
-        </tr>
-        <tr>
-          <td colspan="2">Subtotal</td>
-          <td>$${formattedSubtotal}</td>
-        </tr>
-        <tr>
-          <td colspan="2">Tax</td>
-          <td>$${formattedTax}</td>
-        </tr>
-        <tr>
-          <td colspan="2">Total</td>
-          <td>$${formattedTotal}</td>
-        </tr>
-      </table>
+      <tr>
+        <th colspan="3">Customer Information</th>
+      </tr>
+      <tr>
+        <td colspan="3">
+          Customer Name: ${name}<br>
+          Street: ${street}<br>
+          City: ${city}, ${state} ${zip}<br>
+          Phone: ${phone}
+        </td>
+      </tr>
+      <tr>
+        <th width="20%">Item</th>
+        <th>Product</th>
+        <th width="20%">Price</th>
+      </tr>
+      <tr>
+        <td>1</td>
+        <td>${item1}</td>
+        <td>$${price1}</td>
+      </tr>
+      <tr>
+        <td>2</td>
+        <td>${item2}</td>
+        <td>$${price2}</td>
+      </tr>
+      <tr>
+        <td>3</td>
+        <td>${item3}</td>
+        <td>$${price3}</td>
+      </tr>
+      <tr>
+        <td></td>
+        <td>Subtotal</td>
+        <td>$${formattedSubtotal}</td>
+      </tr>
+      <tr>
+        <td></td>
+        <td>Tax</td>
+        <td>$${formattedTax}</td>
+      </tr>
+      <tr>
+        <td></td>
+        <td>Total</td>
+        <td>$${formattedTotal}</td>
+      </tr>
+    </table>
+    
     `;
   
     // Open a new window and write the invoice content
